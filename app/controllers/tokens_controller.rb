9b67bab67d3e04c2554ab4dd9189794a84939697
chundo <segundo.espana1@gmail.com>
2018-02-10 18:23:19 -0500

    Configuraciion del controlador tokens

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -2,7 +2,7 @@ class TokensController < ApplicationController
   
   def create
   	user = User.find_by(username: params[:username])
-  	byebug
+	#byebug
   	if user&.authenticate(params[:password])
   		render json: {
   			jwt: encode_token({id: user.id, username: user.username })
